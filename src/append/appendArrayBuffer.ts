03c1ac5f505afb6c474514be7dcb89db3d9149d6
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2025-07-23 14:09:57 +0200

    fix: complete buffer types supported by `appendArrayBuffer` (#32)
    Refs: https://github.com/zakodium/nmrium/pull/341#discussion_r2225238087

--- src/append/appendArrayBuffer.ts
+++ src/append/appendArrayBuffer.ts
@@ -2,10 +2,20 @@ import type { ExtendedSourceItem } from '../ExtendedSourceItem.ts';
 import type { FileCollection } from '../FileCollection.ts';
 import { getNameInfo } from '../utilities/getNameInfo.ts';
 
+type SyncOrAsync<T> = T | Promise<T>;
+export type SupportedBufferInput = SyncOrAsync<
+  // most generic binary data manipulation
+  | Uint8Array
+  // generic binary data transfer
+  | ArrayBuffer
+  | ArrayBufferView
+>;
+
+// eslint-disable-next-line jsdoc/require-jsdoc
 export async function appendArrayBuffer(
   fileCollection: FileCollection,
   relativePath: string,
-  arrayBuffer: ArrayBuffer | Promise<ArrayBuffer>,
+  arrayBuffer: SupportedBufferInput,
   options: { dateModified?: number } = {},
 ) {
   const source = await getExtendedSourceFromArrayBuffer(
@@ -18,7 +28,7 @@ export async function appendArrayBuffer(
 
 async function getExtendedSourceFromArrayBuffer(
   originalRelativePath: string,
-  arrayBuffer: ArrayBuffer | Promise<ArrayBuffer | Uint8Array> | Uint8Array,
+  arrayBuffer: SupportedBufferInput,
   options: { dateModified?: number } = {},
 ): Promise<ExtendedSourceItem> {
   const { name, relativePath } = getNameInfo(originalRelativePath);
