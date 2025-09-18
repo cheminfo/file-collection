dca145918375e2ae9f45dc83921b4b7f0a278177
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2025-09-18 16:12:26 +0200

    chore: remove useless describe
    Refs: https://github.com/cheminfo/file-collection/pull/41#discussion_r2359445912

--- src/__tests__/FileCollection.toZip.test.ts
+++ src/__tests__/FileCollection.toZip.test.ts
@@ -3,33 +3,31 @@ import { join } from 'node:path';
 import { Readable } from 'node:stream';
 
 import { Uint8ArrayReader, ZipReader } from '@zip.js/zip.js';
-import { describe, expect, it } from 'vitest';
+import { expect, test } from 'vitest';
 
 import { FileCollection } from '../FileCollection.js';
 
-describe('FileCollection toZip', () => {
-  it('Properly repack with external sources', async () => {
-    const url = 'https://image-js.github.io/image-dataset-demo/biology/';
-    const fileCollection = await new FileCollection().appendWebSource(url);
+test('Properly repack with external sources', async () => {
+  const url = 'https://image-js.github.io/image-dataset-demo/biology/';
+  const fileCollection = await new FileCollection().appendWebSource(url);
 
-    const zip = await fileCollection.toZip();
-    const zipReader = new ZipReader(new Uint8ArrayReader(zip));
-    const entries = await zipReader.getEntries();
+  const zip = await fileCollection.toZip();
+  const zipReader = new ZipReader(new Uint8ArrayReader(zip));
+  const entries = await zipReader.getEntries();
 
-    expect(entries).toHaveLength(3);
-  });
+  expect(entries).toHaveLength(3);
+});
 
-  it('properly repack with recursive zip', async () => {
-    const stream = createReadStream(
-      join(import.meta.dirname, 'dataRecursiveZip/data.zip'),
-    );
-    const webStream = Readable.toWeb(stream);
-    const fileCollection = await FileCollection.fromZip(webStream);
+test('properly repack with recursive zip', async () => {
+  const stream = createReadStream(
+    join(import.meta.dirname, 'dataRecursiveZip/data.zip'),
+  );
+  const webStream = Readable.toWeb(stream);
+  const fileCollection = await FileCollection.fromZip(webStream);
 
-    const zip = await fileCollection.toZip();
-    const zipReader = new ZipReader(new Uint8ArrayReader(zip));
-    const entries = await zipReader.getEntries();
+  const zip = await fileCollection.toZip();
+  const zipReader = new ZipReader(new Uint8ArrayReader(zip));
+  const entries = await zipReader.getEntries();
 
-    expect(entries).toHaveLength(1);
-  });
+  expect(entries).toHaveLength(1);
 });
