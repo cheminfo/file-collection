f3b34c359ac9d546e21893999583a460ab8b023a
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2025-11-04 10:13:53 +0100

    test: add tests involving external data

--- src/__tests__/file_collection.subroot.test.ts
+++ src/__tests__/file_collection.subroot.test.ts
@@ -1,4 +1,18 @@
-import { expect, test } from 'vitest';
+import { createReadStream } from 'node:fs';
+import { join } from 'node:path';
+
+import { HttpResponse, http } from 'msw';
+import { setupServer } from 'msw/node';
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  test,
+} from 'vitest';
 
 import { FileCollection } from '../FileCollection.ts';
 
@@ -52,4 +66,107 @@ test('should not include "subPath" file', async () => {
 
   expect(sources).toStrictEqual(expected);
   expect(files).toStrictEqual(expected);
+});
+
+describe('web source', () => {
+  let requestCounter: number;
+  const server = setupServer(
+    http.get('http://localhost/*', (ctx) => {
+      const basePath = join(import.meta.dirname, 'webSource');
+      const url = new URL(ctx.request.url);
+      const stream = createReadStream(join(basePath, url.pathname));
+      requestCounter++;
+
+      return new HttpResponse(stream, { status: 200 });
+    }),
+  );
+
+  beforeAll(() => server.listen());
+
+  beforeEach(() => {
+    requestCounter = 0;
+  });
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => server.close());
+
+  it('should works with mixed data sources', async () => {
+    const collection = new FileCollection();
+    await collection.appendWebSource('http://localhost/self/index.json');
+    await collection.appendWebSource('http://localhost/other/index.json');
+    await collection.appendText('self/foo.txt', 'bar');
+    await collection.appendText('other/bar.txt', 'baz');
+    const self = collection.subroot('self').alphabetical();
+    const sources = self.sources.map(mapRelativePath);
+    const files = self.files.map(mapRelativePath);
+    const expected = ['dir1/a.txt', 'dir2/b.txt', 'foo.txt'].toSorted();
+
+    expect(sources).toStrictEqual(expected);
+    expect(files).toStrictEqual(expected);
+  });
+
+  // eslint-disable-next-line unicorn/consistent-function-scoping
+  async function getCollectionWithMixedData() {
+    const _self = new FileCollection();
+    await _self.appendWebSource('http://localhost/self/index.json');
+
+    const _other = new FileCollection();
+    await _other.appendWebSource('http://localhost/other/index.json');
+
+    const collection = new FileCollection();
+
+    collection.appendFileCollection(_self, 'self');
+    await collection.appendText('self/foo.txt', 'bar');
+
+    collection.appendFileCollection(_other, 'other');
+    await collection.appendText('other/bar.txt', 'baz');
+
+    // ensure consume files for requestCounter
+    await Promise.all(collection.files.map((f) => f.arrayBuffer()));
+
+    return collection;
+  }
+
+  it('should be able to serialize / deserialize mixed data sources with includeData: false', async () => {
+    const collection = await getCollectionWithMixedData();
+    const self = collection.subroot('self').alphabetical();
+    const iumBuffer = await self.toIum({ includeData: false });
+
+    expect(requestCounter).toBe(6); // web-sources (2) + entries (4)
+
+    const ium = await FileCollection.fromIum(iumBuffer);
+    ium.alphabetical();
+    await Promise.all(ium.files.map((f) => f.arrayBuffer()));
+
+    expect(requestCounter).toBe(10); // fetch entries
+
+    const sources = ium.sources.map(mapRelativePath);
+    const files = ium.files.map(mapRelativePath);
+    const expected = ['dir1/a.txt', 'dir2/b.txt', 'foo.txt'].toSorted();
+
+    expect(sources).toStrictEqual(expected);
+    expect(files).toStrictEqual(expected);
+  });
+
+  it('should be able to serialize / deserialize mixed data sources with includeData: true', async () => {
+    const collection = await getCollectionWithMixedData();
+    const self = collection.subroot('self').alphabetical();
+    const iumBuffer = await self.toIum({ includeData: true });
+
+    expect(requestCounter).toBe(6); // web-sources (2) + entries (4)
+
+    const ium = await FileCollection.fromIum(iumBuffer);
+    ium.alphabetical();
+    await Promise.all(ium.files.map((f) => f.arrayBuffer()));
+
+    expect(requestCounter).toBe(6); // no fetch entries, there are embed into the archive
+
+    const sources = ium.sources.map(mapRelativePath);
+    const files = ium.files.map(mapRelativePath);
+    const expected = ['dir1/a.txt', 'dir2/b.txt', 'foo.txt'].toSorted();
+
+    expect(sources).toStrictEqual(expected);
+    expect(files).toStrictEqual(expected);
+  });
 });
