a4932ec80d415ba83fdbd5f7bb2202a0ee9de3ec
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2025-11-15 11:13:02 +0100

    refactor: move isZip method

--- src/utilities/expand/fileItemUnzip.ts
+++ src/utilities/expand/fileItemUnzip.ts
@@ -2,6 +2,7 @@ import type { FileItem } from '../../FileItem.ts';
 import type { Options } from '../../Options.ts';
 import { defaultOptions } from '../../Options.ts';
 import { shouldAddItem } from '../shouldAddItem.ts';
+import { isZip } from '../zip.ts';
 
 import { expandAndFilter } from './expandAndFilter.ts';
 import { fileItemsFromZip } from './fileItemsFromZip.ts';
@@ -58,15 +59,4 @@ export async function fileItemUnzip(
   }
 
   return fileItems;
-}
-
-function isZip(buffer: ArrayBufferLike): boolean {
-  if (buffer.byteLength < 5) return false;
-  const bytes = new Uint8Array(buffer);
-  return (
-    bytes[0] === 0x50 &&
-    bytes[1] === 0x4b &&
-    (bytes[2] === 0x03 || bytes[2] === 0x05 || bytes[2] === 0x07) &&
-    (bytes[3] === 0x04 || bytes[3] === 0x06 || bytes[3] === 0x08)
-  );
 }
