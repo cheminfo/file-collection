f6a6f28af2a14c3d3d8f432b0f86c38d7a8bceab
tpoisseau <22891227+tpoisseau@users.noreply.github.com>
2025-11-15 12:42:02 +0100

    fix: isZip implementation
    It validated false zip signatures.
    Improve performance, it check a single number instead separated bytes.

--- src/utilities/zip.ts
+++ src/utilities/zip.ts
@@ -1,10 +1,24 @@
+const SIGNATURE_NORMAL = 0x504b0304; // PK\x03\x04
+const SIGNATURE_EMPTY = 0x504b0506; // PK\x05\x06
+const SIGNATURE_SPANNED = 0x504b0708; // PK\x07\x08
+const SIGNATURES = new Set([
+  SIGNATURE_NORMAL,
+  SIGNATURE_EMPTY,
+  SIGNATURE_SPANNED,
+]);
+
+/**
+ * Fast check if the buffer is a zip file
+ * @param buffer - the buffer to check
+ * @returns boolean
+ */
 export function isZip(buffer: ArrayBufferLike): boolean {
-  if (buffer.byteLength < 5) return false;
-  const bytes = new Uint8Array(buffer);
-  return (
-    bytes[0] === 0x50 &&
-    bytes[1] === 0x4b &&
-    (bytes[2] === 0x03 || bytes[2] === 0x05 || bytes[2] === 0x07) &&
-    (bytes[3] === 0x04 || bytes[3] === 0x06 || bytes[3] === 0x08)
-  );
+  // https://pkware.cachefly.net/webdocs/casestudies/APPNOTE.TXT#:~:text=4.3.16%20end%20of%20central%20directory%20record%3A
+  // empty zip file has minimal 22 bytes size (it's only an "end of central directory record")
+  // normal / spanned zip files have minimal 30 bytes
+  if (buffer.byteLength < 22) return false;
+
+  const view = new DataView(buffer);
+  const signature = view.getUint32(0, false);
+  return SIGNATURES.has(signature);
 }
